88747b45a02118c21fec872e7a3e0d691a3f78ef
Denis Galkin <dengal77@yandex.ru>
2023-06-25 17:03:43 +0300

    homework1 completed correction1

--- task08/Program.cs
+++ task08/Program.cs
@@ -5,12 +5,20 @@
 Console.WriteLine("Введите число:");
 int number = Convert.ToInt32(Console.ReadLine());
 int count = 1;
-Console.Write($"Четные цифры при введеной цифре {number} будут ");
-while (count <= number)
+if (number > 0)
 {
-    if (count % 2 == 0)
+    Console.Write($"Четные цифры при введеной цифре {number} будут ");
+    while (count <= number)
     {
-        Console.Write($"{count} ");
+        if (count % 2 == 0)
+        {
+            Console.Write($"{count} ");
+        }
+        count = count + 1;
     }
-    count = count + 1;
-}
+}   
+ else
+ {
+    Console.Write("Ошибка ввода, введите положительное число большее нуля");
+ }
+    
