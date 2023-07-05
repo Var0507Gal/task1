00c7508937ea8081f6749cc6a6370bd2b2faff51
Denis Galkin <dengal77@yandex.ru>
2023-07-05 21:50:28 +0300

    homework4

---
+++ task24/Program.cs
@@ -0,0 +1,17 @@
+﻿// задача 24
+// Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А
+Console.WriteLine("Введите целое число A: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumNumber = SumNumber(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} равна {sumNumber}");
+int SumNumber(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i;
+    }  
+    return sum;
+}
---
+++ task29/Program.cs
@@ -0,0 +1,14 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Задача 29: Напишите программу, которая задаёт
+// массив из 8 элементов, заполненный
+// псевдослучайными числами и выводит их на экран.
+
+int n = 8;
+int[] arr = new int[n];
+Random rand = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next(0,2);
+}
+Console.WriteLine(string.Join(", ", arr));
