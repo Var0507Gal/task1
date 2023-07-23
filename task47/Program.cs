c521e2a7061a3a6c1948a841365aca2d1ec1bc9b
Denis Galkin <dengal77@yandex.ru>
2023-07-23 13:17:05 +0300

    homework corrected for remove

---
+++ task47/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+
+Console.WriteLine("введите количество строк");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
